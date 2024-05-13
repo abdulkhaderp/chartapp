c2e1b93ee9abd4e023259760dc0b3234401562b4
Abdul khader P <abdulkhaderp@gmail.com>
2024-05-13 10:53:24 +0530

    abc

---
+++ src/Circle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Circle.css'; // Import CSS file for styling
+
+const Circle = () => {
+  return (
+    <div className="circle">
+      <div className="icon">icon</div>
+      <div className="text">100%</div>
+    </div>
+  );
+};
+
+export default Circle;
